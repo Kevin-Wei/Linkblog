76a83955680e1d468100485cb90ef6c2f6fefdab
Kevin Wei <kevin.tw.wei@gmail.com>
2013-05-10 14:52:41 -0700

    initial linkblog from scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkblog::Application.config.secret_token = '7af30384641e86b83e8c60c699bb67ffd226d7f50899896943a90224bca47256571a87f3a77b374245617d2ace86fbc24f6997902dca49ea0866e7d98164259b'
